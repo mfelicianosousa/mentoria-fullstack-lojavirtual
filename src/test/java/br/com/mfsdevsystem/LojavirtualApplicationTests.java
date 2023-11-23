e1c3a184da69185c931b961ff831ff61d063fd71
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2023-11-23 14:39:07 -0400

    Commit Inicial Projeto Mentoria JavaSpring Boot

---
+++ src/test/java/br/com/mfsdevsystem/LojavirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mfsdevsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojavirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
