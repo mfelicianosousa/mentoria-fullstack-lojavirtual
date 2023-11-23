9ccc24f6afc7f3283c2bfb2fd79ae1ad89f57776
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2023-11-23 17:40:10 -0400

    Classe de Categoria_produto

---
+++ src/main/java/br/com/mfsdevsystem/model/CategoriaProduto.java
@@ -0,0 +1,66 @@
+package br.com.mfsdevsystem.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="categoria_produto")
+@SequenceGenerator(name = "sequence_categoria_produto", sequenceName = "sequence_categoria_produto", allocationSize = 1, initialValue = 1)
+public class CategoriaProduto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequence_categoria_produto")
+	private Long id ;
+	
+	@Column(name="nome", nullable = false )
+	private String nome;
+
+	public CategoriaProduto(Long id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CategoriaProduto other = (CategoriaProduto) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
